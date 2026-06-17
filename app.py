5181a7b18c46e1497bdd23379b2f09f9f0ee1715
Hyma <hymavathi.gonti@gmail.com>
2026-06-17 09:50:25 +0000

    updated app to version 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "AI Job Hunter is Running!"
+    return "AI Job Hunter version 2 - CI/CD Demo"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
